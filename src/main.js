de77152820a39053bba0027d9593501bf21069bc
SamiDZOGANG <sami.dzogangnzogagne@loreal.com>
2025-08-28 04:47:29 +0200

    Create missing index.html entry file

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Carbon</h1>
+    <div class="card">
+      <p>Welcome to Carbon application</p>
+    </div>
+  </div>
+`
